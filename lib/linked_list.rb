39fdc1f0a7eb4670252ea18c2d90f8694695305c
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-03 18:59:00 -0500

    impliment linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,39 +1,130 @@
 module Linked
   class List
-    def append
+    def initialize items=[]
+      items.each { |item| self.append item }
     end
 
-    def prepend
+    def each
+      return enum_for :each unless block_given?
+
+      self.each_node { |node| yield node.value }
+    end
+
+    def to_a
+      self.each.to_a
+    end
+
+    def append value
+      self.tail = value
+      self
+    end
+
+    def prepend value
+      self.head = value
+      self
     end
 
     def size
+      self.nodes.size
     end
 
     def head
+      @head.value unless @head === nil
     end
 
     def tail
+      @tail.value unless @head === nil
     end
 
     def at index
+      self.each_node.with_index { |item, i| return item.value if i == index }
     end
 
     def pop
+      tail = @tail
+      self.each_node do |node|
+        next unless node.next_node === @tail
+        @tail = node
+        @tail.next_node = nil
+        break
+      end
+      @head = nil if tail === @head
+      tail.value
     end
 
-    def contains?
+    def contains? *args
+      self.to_a.include? *args
     end
 
-    def find data
+    def find *args
+      self.each.find_index *args
     end
 
     def to_s
+      return 'nil' if @head === nil
+      "#{self.nodes.join(' -> ')} -> nil"
     end
 
-    def insert_at index
+    def insert_at index, value
+      return self.prepend value if index == 0
+
+      self.each_node.with_index do |node, i|
+        next unless i == index - 1
+        return self.append value if node === @tail
+
+        next_node = Node.new value, node.next_node
+        node.next_node = next_node
+        break
+      end
+      self
     end
 
     def remove_at index
+      (self.shift; return self) if index == 0
+
+      self.each_node.with_index do |node, i|
+        next unless i == index - 1
+        (self.pop; return self) if node.next_node === @tail
+
+        node.next_node = node.next_node.next_node
+
+        break
+      end
+      self
+    end
+
+    protected
+
+    def each_node
+      return enum_for :each_node unless block_given?
+
+      node = @head
+      until node === nil
+        yield node
+        node = node.next_node
+      end
+    end
+
+    def nodes
+      self.each_node.to_a
+    end
+
+    def shift
+      @head, head = @head.next_node, @head
+      head
+    end
+
+    def head= value
+      @head = Node.new value, @head
+      @tail = @head if @head.next_node === nil
+      self
+    end
+
+    def tail= value
+      return self.head = value if @tail === nil
+
+      @tail.next_node = Node.new value
+      @tail = @tail.next_node
     end
   end
 
@@ -43,6 +134,10 @@ module Linked
     def initialize value=nil, next_node=nil
       @value = value
       @next_node = next_node
+    end
+
+    def to_s
+      "( #{@value.to_s} )"
     end
   end
 end
--- spec/lib/linked_list_spec.rb
+++ spec/lib/linked_list_spec.rb
@@ -5,17 +5,55 @@ include Linked
 describe Linked::List do
   before :each do
     @empty_list = List.new
+    @list = List.new (1..5)
   end
+
+  describe "#initialize" do
+    it "should exist" do
+      @empty_list.should be_instance_of List
+    end
+    it 'should initialize with an optional list' do
+      @list.head.should eq 1
+      @list.tail.should eq 5
+    end
+  end
+
 
   describe "#append" do
     it "should exist" do
       @empty_list.respond_to?(:append).should be true
     end
+
+    it 'should add item to end of list when list is empty' do
+      @empty_list.append 1
+      @empty_list.head.should eq 1
+      @empty_list.tail.should eq 1
+    end
+
+    it 'should add item to end of list when list is not empty' do
+      @empty_list.append(1).append(2).append(3)
+      @empty_list.head.should eq 1
+      @empty_list.tail.should eq 3
+    end
+
   end
 
   describe "#prepend" do
     it "should exist" do
       @empty_list.respond_to?(:prepend).should be true
+    end
+
+    it 'should add a node to the head position when head is nil' do
+      @empty_list.prepend(1)
+      @empty_list.head.should eq 1
+      @empty_list.tail.should eq 1
+    end
+
+    it 'should add a node to the head position when head has a value' do
+      @empty_list.prepend(1).prepend(2).prepend(3)
+
+      @empty_list.head.should eq 3
+      @empty_list.tail.should eq 1
     end
   end
 
@@ -23,11 +61,24 @@ describe Linked::List do
     it "should exist" do
       @empty_list.respond_to?(:size).should be true
     end
+
+    it 'should get empty list size' do
+      @empty_list.size.should eq 0
+    end
+
+    it 'should get  list size' do
+      puts @list.size
+      @list.size.should eq 5
+    end
   end
 
   describe "#head" do
     it "should exist" do
       @empty_list.respond_to?(:head).should be true
+    end
+
+    it 'should initially be nil' do
+      @empty_list.head.should be_nil
     end
   end
 
@@ -35,11 +86,28 @@ describe Linked::List do
     it "should exist" do
       @empty_list.respond_to?(:tail).should be true
     end
+    it 'should initially be nil' do
+      @empty_list.tail.should be_nil
+    end
+
+    it 'should be first element when item is added' do
+      @empty_list.prepend(1).tail.should eq 1
+    end
   end
 
   describe "#at" do
     it "should exist" do
       @empty_list.respond_to?(:at).should be true
+    end
+
+    it 'should return item at index 0' do
+      @list.at(0).should eq 1
+    end
+    it 'should return item at index 3' do
+      @list.at(3).should eq 4
+    end
+    it 'should return item at index 4' do
+      @list.at(4).should eq 5
     end
   end
 
@@ -47,11 +115,47 @@ describe Linked::List do
     it "should exist" do
       @empty_list.respond_to?(:pop).should be true
     end
+
+    it 'should return and remove last item from the list' do
+      item = @list.pop
+      item.should eq 5
+      @list.tail.should eq 4
+    end
+
+    it 'should pop several and still work' do
+      @list.pop
+      @list.pop
+      @list.pop
+
+      @list.tail.should eq 2
+      @list.to_a.should eq [1, 2]
+    end
+
+    it 'should set head and tail to nil if last item' do
+      list = List.new [1]
+      list.pop
+
+      list.head.should be_nil
+      list.tail.should be_nil
+    end
   end
+
 
   describe "#contains?" do
     it "should exist" do
       @empty_list.respond_to?(:contains?).should be true
+    end
+
+    it 'should work on empty list' do
+      @empty_list.contains?(4).should be false
+    end
+
+    it 'should return true if list contains value' do
+      @list.contains?(5).should be true
+    end
+
+    it 'should return false if list does not contain value' do
+      @list.contains?(6).should be false
     end
   end
 
@@ -59,37 +163,160 @@ describe Linked::List do
     it "should exist" do
       @empty_list.respond_to?(:find).should be true
     end
+
+    it 'should find the index for a value' do
+      list = List.new %w[cat dog rabbit fish]
+      list.find('rabbit').should eq 2
+    end
+
+    it 'should return nil for an empty list' do
+      @empty_list.find('rabbit').should be_nil
+    end
   end
 
   describe "#to_s" do
     it "should exist" do
       @empty_list.respond_to?(:to_s).should be true
     end
+    it 'should format values to a string' do
+      list = List.new %w[cat dog rabbit fish]
+      list.to_s.should eq '( cat ) -> ( dog ) -> ( rabbit ) -> ( fish ) -> nil'
+    end
+
+    it 'should return "nil" for an empty list' do
+      @empty_list.to_s.should eq 'nil'
+    end
   end
 
-  xdescribe "#insert_at" do
+  describe "#insert_at" do
     it "should exist" do
       @empty_list.respond_to?(:insert_at).should be true
     end
-  end
 
-  xdescribe "#remove_at" do
-    it "should exist" do
-      @empty_list.respond_to?(:remove_at).should be true
+    it 'should insert value at index 0' do
+      @list.insert_at(0, 'data').to_a.should eq ['data', 1, 2, 3, 4, 5]
+    end
+
+    it 'should insert value at last index' do
+      @list.insert_at(5, 'data').to_a.should eq [1, 2, 3, 4, 5, 'data']
+    end
+
+    it 'should insert value at any index' do
+      @list.insert_at(2, 'data').to_a.should eq [1, 2, 'data', 3, 4, 5]
+    end
+
+    it 'should do nothing if index is out of range' do
+      @list.insert_at(40, 'data').to_a.should eq [1, 2, 3, 4, 5]
+    end
+
+    it 'should do nothing if index is out of range with empty_list' do
+      @empty_list.insert_at(40, 'data').to_a.should eq []
     end
   end
 
+  describe "#remove_at" do
+    it "should exist" do
+      @empty_list.respond_to?(:remove_at).should be true
+    end
+
+    it 'should remove value at index 0' do
+      @list.remove_at(0).to_a.should eq [2, 3, 4, 5]
+    end
+
+    it 'should remove value at last index' do
+      @list.remove_at(4).to_a.should eq [1, 2, 3, 4]
+    end
+
+    it 'should remove value at second to last index' do # potential edge
+      @list.remove_at(3).to_a.should eq [1, 2, 3, 5]
+    end
+
+    it 'should remove value at any index' do
+      @list.remove_at(2).to_a.should eq [1, 2, 4, 5]
+    end
+
+    it 'should should do nothing of index is out of range' do
+      @list.remove_at(40).to_a.should eq [1, 2, 3, 4, 5]
+    end
+
+    it 'should should do nothing of index is out of range with empty_list' do
+      @empty_list.remove_at(40).to_a.should eq []
+    end
+
+    it 'should set head and tail to nil when removing last value' do
+      list = List.new [1]
+      list.remove_at(0)
+
+      list.head.should be_nil
+      list.tail.should be_nil
+      list.to_a.should eq []
+    end
+  end
+
+  describe "#each" do
+    it "should exist" do
+      @empty_list.respond_to?(:each).should be true
+    end
+
+    it 'should be an Enumerator' do
+      @list.each.should be_instance_of Enumerator
+    end
+
+    it 'should work like an enumerable' do
+      enum = @list.each
+      enum.next.should eq 1
+      enum.next.should eq 2
+      enum.next.should eq 3
+    end
+
+    it 'should worth with to_a' do
+      @list.each.to_a.should eq [1, 2, 3, 4, 5]
+    end
+
+    it 'should work with code block' do
+      items = []
+      @list.each { |n| items << n }
+
+      items.should eq [1, 2, 3, 4, 5]
+    end
+  end
+
+  describe "#to_a" do
+    it "should exist" do
+      @empty_list.respond_to?(:to_a).should be true
+    end
+
+    it 'should return an array of items' do
+      @list.to_a.should eq [1, 2, 3, 4, 5]
+    end
+  end
 end
 
 describe Linked::Node do
+  describe '#initialize' do
+    before(:each) do
+      @node = Node.new
+    end
+    it 'should have initial value of nil' do
+      @node.value.should equal nil
+    end
 
-  it 'should have initial value of nil' do
-    node = Node.new
-    node.value.should equal nil
+    it 'should have initial next_node value of nil' do
+      @node.next_node.should equal nil
+    end
   end
 
-  it 'should have initial next_node value of nil' do
-    node = Node.new
-    node.next_node.should equal nil
+  describe '#to_s' do
+    before(:each) do
+    end
+
+    it 'should return its value' do
+      node = Node.new 10
+      node.to_s.should eq '( 10 )'
+    end
+
+
   end
+
+
 end
