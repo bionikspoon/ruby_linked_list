434ef7c0d95f34a8c9eabc0d15a2ae9aafb9633d
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-03 02:20:10 -0500

    Initial Commit

---
+++ lib/linked_list.rb
@@ -0,0 +1,6 @@
+module Linked
+  class List
+  end
+  class Node
+  end
+end
---
+++ spec/lib/linked_list_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'linked_list'
+
+describe Linked::List do
+  it 'should do something' do
+
+    true.should == false
+  end
+end
+describe Linked::Node do
+end
